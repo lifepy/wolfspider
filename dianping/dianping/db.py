d0ad4e3497f78f7b76c68e5b55177415dca851ab
Han Zhang <zhanghan.simon@gmail.com>
2011-06-08 10:27:29 -0400

    modified db to whaleshark

--- dianping/dianping/db.py
+++ dianping/dianping/db.py
@@ -1,7 +1,8 @@
 import pymongo
 
+whaleshark = '128.210.189.88'
 def get_connection():
-    conn = pymongo.Connection().dianping
+    conn = pymongo.Connection(whaleshark).dianping
     conn.authenticate('dianping','crawler')
     return conn
 
